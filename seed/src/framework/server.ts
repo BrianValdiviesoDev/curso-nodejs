e2cda4de96a59fa7ac685e93639012c82cec21b1
Brian Valdivieso <brian.valdivieso.dev@gmail.com>
2025-03-15 13:24:39 +0100

    Improve seed

--- seed/src/framework/server.ts
+++ seed/src/framework/server.ts
@@ -1,14 +1,17 @@
-import express from "express";
-import sampleRouter from "./routes/sample.routes"
+import express, {Express} from "express";
 import { ErrorHandler } from "./middlewares/errorHandler";
 import { logger } from "./middlewares/logger";
+import sampleRouter from "./routes/sample.routes"
 
-const app = express()
-app.use(express.json())
+const app = async (): Promise<Express> => {
+    const app = express()
+    app.use(express.json())
 
-app.use('/api/sample', sampleRouter)
+    app.use('/api/sample', sampleRouter)
 
-app.use(ErrorHandler.handle)
-app.use(logger)
+    app.use(ErrorHandler.handle)
+    app.use(logger)
+    return app
 
+}
 export default app;
